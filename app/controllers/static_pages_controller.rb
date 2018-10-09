378e97bdf2870f3bcdfdd5afb1fb83b153308c7e
Darhan Rzaev <darhanrzaev@yahoo.com>
2018-10-09 11:25:43 -0400

    modified

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def subjects
+  end
+
+  def courses
+  end
+
+  def instructors
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  resources :users
-  root 'users#index'
+  #get 'static_pages/home'
+  #resources :user
+  #root 'users#index'
+  #root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+  get  '/subjects',    to: 'static_pages#subjects'
+  get  '/courses',   to: 'static_pages#courses'
+  get  '/instructors', to: 'static_pages#instructors'
 end
