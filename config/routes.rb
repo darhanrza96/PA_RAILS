fe86b925ae049a30113b241cb5a07437742d64d4
Darhan Rzaev <darhanrzaev@yahoo.com>
2018-10-09 02:31:34 -0400

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'static_pages/home'
+  resources :users
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
