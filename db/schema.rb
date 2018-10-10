f88fe12bad86cb0779acd6e12f51fdfbdb4d82d5
Darhan Rzaev <darhanrzaev@yahoo.com>
2018-10-09 22:48:23 -0400

    good version

--- db/schema.rb
+++ db/schema.rb
@@ -9,14 +9,3 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2018_10_09_050425) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-end
