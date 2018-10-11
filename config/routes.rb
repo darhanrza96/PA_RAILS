4ffe132d3d8baa4af119a14e801799069412061c
Darhan Rzaev <darhanrzaev@yahoo.com>
2018-10-10 22:51:24 -0400

    modified new

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
+  
   get  '/subjects',    to: 'static_pages#subjects'
   get  '/courses',   to: 'static_pages#courses'
   get  '/instructors', to: 'static_pages#instructors'
