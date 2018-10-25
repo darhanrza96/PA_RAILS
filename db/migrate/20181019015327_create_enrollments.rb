07e1e01fd182aa181b1fdc5703fd9e0a95e531bb
Darhan Rzaev <darhanrzaev@yahoo.com>
2018-10-25 19:33:55 -0400

    modified

--- db/migrate/20181019015327_create_enrollments.rb
+++ db/migrate/20181019015327_create_enrollments.rb
@@ -1,8 +1,5 @@
 class CreateEnrollments < ActiveRecord::Migration[5.2]
-  def change
-    create_table :users do |t|
-      t.string :name
-    end
+
 
     create_table :courses do |t|
       t.string :name
